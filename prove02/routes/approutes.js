26080572864697879f941a6966e8eeb501cca619
Flavio Prumucena <flaprumucena@byui.edu>
2021-04-30 00:22:36 -0600

    finished all core requirements

--- prove02/routes/approutes.js
+++ prove02/routes/approutes.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const bookList = [];
 
-
-router.get('/', (req, res, next) => {
-    res.render('pages/home', {
+router.get('/add-book', (req, res, next) => {
+    res.render('pages/addbook', {
         title: 'Team Activity 02',
-        path: 'home'
+        path: 'addbook'
+    });
+});
 
+router.post('/save-book', (req, res, next) => {
+    const book = {
+        title: req.body.book,
+        summary: req.body.summary
+    };
+    bookList.push(book);
+    res.redirect('/');
+});
+
+router.use('/', (req, res, next) => {
+    res.render('pages/home', {
+        path: 'home',
+        books: bookList
     });
 });
 
